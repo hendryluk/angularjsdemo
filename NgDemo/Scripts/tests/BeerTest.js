e7a612fc65db1d5f4f102c4436271097f4acc9c7
Hendry Luk <hendrymail@gmail.com>
2013-03-15 11:32:33 +1100

    Step1

--- NgDemo/Scripts/tests/BeerTest.js
+++ NgDemo/Scripts/tests/BeerTest.js
@@ -1,1 +1,12 @@
-﻿///<reference path="../Apps/BeerController.js"/>
+﻿///<reference path="../angular-mocks.js"/>
+///<reference path="../Apps/BeerController.js"/>
+describe("Beer controller", function() {
+    it("Can shout", function() {
+        var $scope = {};
+        BeerController($scope);
+
+        $scope.message = "sheep";
+        $scope.shout();
+        expect($scope.message).toBe("SHEEP!!");
+    });
+})

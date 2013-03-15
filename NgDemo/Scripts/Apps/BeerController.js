400a35e9bb22d0e6723163c9b87aeb573dd551ce
Hendry Luk <hendrymail@gmail.com>
2013-03-15 13:33:58 +1100

    step4

--- NgDemo/Scripts/Apps/BeerController.js
+++ NgDemo/Scripts/Apps/BeerController.js
@@ -1,12 +1,32 @@
 ﻿angular.module("presso", []).config(function ($routeProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
+    //$locationProvider.html5Mode(true);
     $routeProvider
         .when('/hello/:msg', { templateUrl: "/Scripts/Templates/hello.html", controller: BeerController })
         .otherwise({redirectTo: "/hello/Waddup"});
-});
+})
+//    .directive("datepicker", function () {
+//    return {
+//        link: function($scope, element) {
+//            element.datepicker();
+//        }
+//    };
+//})
+    .directive("alert", function() {
+        return {
+            restrict:"E",
+            templateUrl: "/Scripts/Templates/alert.html",
+            scope: {
+                title: "@",
+                text: "=",
+                onOk: "&"
+            }
+        };
+    });
 
 function BeerController($scope, $http, $routeParams) {
     $scope.message = $routeParams.msg;
+
+    $scope.partyDate = '1/1/2014';
 
     $scope.shout = function () {
         this.message = this.message.toUpperCase() + "!!";

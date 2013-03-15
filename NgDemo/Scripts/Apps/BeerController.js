d67064a8f26549ea9dc818ffe8a03b56974594a2
Hendry Luk <hendrymail@gmail.com>
2013-03-15 11:33:12 +1100

    step1

--- NgDemo/Scripts/Apps/BeerController.js
+++ NgDemo/Scripts/Apps/BeerController.js
@@ -1,1 +1,7 @@
-﻿
+﻿function BeerController($scope, $http) {
+    $scope.message = "hello world";
+
+    $scope.shout = function() {
+        this.message = this.message.toUpperCase() + "!!";
+    };
+}

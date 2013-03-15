e09013da777a58a4fb760d4be2bdd021708b501c
Hendry Luk <hendrymail@gmail.com>
2013-03-15 11:34:53 +1100

    step2

--- NgDemo/Scripts/Apps/BeerController.js
+++ NgDemo/Scripts/Apps/BeerController.js
@@ -4,4 +4,10 @@
     $scope.shout = function() {
         this.message = this.message.toUpperCase() + "!!";
     };
+    
+    $scope.beerPrice = function() {
+        $http.get("/Home/BeerPrice").success(function(data) {
+            $scope.message = "Beer price: " + data.MarketPrice;
+        });
+    }
 }
--- NgDemo/Scripts/tests/BeerTest.js
+++ NgDemo/Scripts/tests/BeerTest.js
@@ -1,4 +1,5 @@
-﻿///<reference path="../Apps/BeerController.js"/>
+﻿///<reference path="../angular-mocks.js"/>
+///<reference path="../Apps/BeerController.js"/>
 describe("Beer controller", function() {
     it("Can shout", function() {
         var $scope = {};
@@ -8,4 +9,19 @@ describe("Beer controller", function() {
         $scope.shout();
         expect($scope.message).toBe("SHEEP!!");
     });
+    
+
+    it("Can get beer price", function() {
+        inject(function($http, _$httpBackend_) {
+            _$httpBackend_.expectGET("/Home/BeerPrice").respond({ MarketPrice: 50 });
+
+            var $scope = {};
+            BeerController($scope, $http);
+
+            $scope.beerPrice();
+            
+            _$httpBackend_.flush();
+            expect($scope.message).toBe("Beer price: 50");
+        });
+    })
 })
